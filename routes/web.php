77b835a514bb31296d97be000cd4b890e19eae63
Luthfi <luthfirobit@gmail.com>
2021-01-20 13:53:03 +0700

    add all checkout route

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,30 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 //     return view('welcome');
 // });
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/detail', 'DetailController@index')->name('detail');
-Route::get('/checkout', 'CheckoutController@index')->name('checkout');
-Route::get('/checkout/success', 'CheckoutController@success')->name('checkout-success');
+
+Route::get('/detail/{slug}', 'DetailController@index')->name('detail');
+
+Route::post('/checkout/{id}', 'CheckoutController@process')
+    ->name('checkout_process')
+    ->middleware(['auth','verified']);
+
+Route::get('/checkout/{id}', 'CheckoutController@index')
+    ->name('checkout')
+    ->middleware(['auth','verified']);
+
+Route::post('/checkout/create/{detail_id}', 'CheckoutController@create')
+    ->name('checkout_create')
+    ->middleware(['auth','verified']);
+
+Route::get('/checkout/remove/{detail_id}', 'CheckoutController@remove')
+    ->name('checkout_remove')
+    ->middleware(['auth','verified']);
+
+Route::get('/checkout/confirm/{id}', 'CheckoutController@success')
+    ->name('checkout_success')
+    ->middleware(['auth','verified']);
+
+// Route::get('/checkout/success', 'CheckoutController@success')->name('checkout-success');
 
 Route::prefix('admin')
     ->namespace('Admin')
